30aafcd3e76a62be9ab52325311fcd9936d4c0db
wang <729901489@qq.com>
2018-08-23 14:11:42 +0800

    修改部分

--- JLPhotoBrowser/down/JLImgCache.m
+++ JLPhotoBrowser/down/JLImgCache.m
@@ -7,7 +7,7 @@
 //
 
 #import "JLImgCache.h"
-#import<CommonCrypto/CommonDigest.h>
+
 #define ImageviewPAth @"imageViews"
 @interface JLImgCache()
 @property (strong, nonatomic, nonnull) NSFileManager *fileManager;
@@ -70,16 +70,16 @@ static JLImgCache *instance;
     //self.fileManager =[NSFileManager defaultManager];
 }
 - (NSString *) md5:(NSString *) input {
-    const char *cStr = [input UTF8String];
-    unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+//    const char *cStr = [input UTF8String];
+//    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+//    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+//
+//    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+//
+//    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+//        [output appendFormat:@"%02x", digest[i]];
     
-    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
-    
-    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
-        [output appendFormat:@"%02x", digest[i]];
-    
-    return  output;
+    return  input;
 }
 
 -(NSString *)getPathForKey:(NSString *)key{
