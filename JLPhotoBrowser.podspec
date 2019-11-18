0da4d6e79a1943fc6854048ec32b66e468db268c
hewei001 <815502919@qq.com>
2019-11-18 17:42:57 +0800

    456

--- JLPhotoBrowser.podspec
+++ JLPhotoBrowser.podspec
@@ -1,29 +1,42 @@
 #
-#  Be sure to run `pod spec lint JLPhotoBrowser.podspec' to ensure this is a
-#  valid spec and to remove all comments including this before submitting the spec.
+# Be sure to run `pod lib lint JLPhotoBrowser.podspec' to ensure this is a
+# valid spec before submitting.
 #
-#  To learn more about Podspec attributes see http://docs.cocoapods.org/specification.html
-#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
 #
 
 Pod::Spec.new do |s|
+  s.name             = 'JLPhotoBrowser'
+  s.version          = '1.0.0'
+  s.summary          = 'A short description of JLPhotoBrowser.'
 
+# This description is used to generate tags and improve search results.
+#   * Think: What does it do? Why did you write it? What is the focus?
+#   * Try to keep it short, snappy and to the point.
+#   * Write the description between the DESC delimiters below.
+#   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.name         = 'JLPhotoBrowser'
-  s.version      = '1.0.0'
-  s.summary      = 'JLPhotoBrowser'
-  s.frameworks = "Foundation", "UIKit","UIKit/UIKit.h"
-  s.description  = <<-DESC
-一个简单的图片浏览JLPhotoBrowser
-                   DESC
+  s.description      = <<-DESC
+TODO: Add long description of the pod here.
+                       DESC
 
-s.homepage     = 'https://github.com/hewei001/JLPhotoBrowser'
+  s.homepage         = 'https://github.com/hewei001/JLPhotoBrowser'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'hw' => '815502919@qq.com' }
+  s.source           = { :git => 'https://github.com/hewei001/JLPhotoBrowser', :tag => s.version.to_s }
+  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.license      = 'MIT'
+  s.ios.deployment_target = '8.0'
 
-  s.author             = { "wang" => "729901489@qq.com" }
-  s.source       = { :git => 'https://github.com/hewei001/JLPhotoBrowser.git', :tag => "#{s.version}" }
-  s.source_files  ="JLPhotoBrowser" "JLPhotoBrowser/class/*.{h,m}"
-  s.requires_arc = true
+  s.source_files = "JLPhotoBrowser/JLPhotoBrowser/*.{h,m}"
+  
+  # s.resource_bundles = {
+  #   'JLPhotoBrowser' => ['JLPhotoBrowser/Assets/*.png']
+  # }
 
+  # s.public_header_files = 'Pod/Classes/**/*.h'
+  # s.frameworks = 'UIKit', 'MapKit'
+  # s.dependency 'AFNetworking', '~> 2.3'
 end
--- JLPhotoBrowser/ViewController.m
+++ JLPhotoBrowser/ViewController.m
@@ -7,12 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "JLPhotoBrowserView.h"
-#import "UIImageView+WebCache.h"
-#import "JLPhotoBrowser.h"
-#import "UIImageView+JLloadimg.h"
-#import "testtableViewController.h"
-#import "JLImgCache.h"
+
 @interface ViewController ()<UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *img1;
 @property (weak, nonatomic) IBOutlet UIImageView *img2;
@@ -25,72 +20,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [_img1 JLloadimgwith:@"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg" andplaceholderImage:@"12345"];
- //   [_img1 sd_setImageWithURL:[NSURL URLWithString:@"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg"]];
-    _img1.userInteractionEnabled =YES;
-    _img1.contentMode = UIViewContentModeScaleAspectFit;
-   [_img1 addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tagclick:)]];
-   
-    [_img2 JLloadimgwith:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=60332803,1042811346&fm=202&mola=new&crop=v1" andplaceholderImage:@"12345"];
-  //  [_img2 sd_setImageWithURL:[NSURL URLWithString:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=60332803,1042811346&fm=202&mola=new&crop=v1"]];
-    _img2.userInteractionEnabled =YES;
-    _img2.contentMode = UIViewContentModeScaleAspectFit;
-    [_img2 addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tagclick:)]];
-    
-    
-    [_img3 JLloadimgwith:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=880709080,1721359692&fm=202&mola=new&crop=v1" andplaceholderImage:@"12345"];
-    //[_img3 sd_setImageWithURL:[NSURL URLWithString:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=880709080,1721359692&fm=202&mola=new&crop=v1"]];
-    _img3.userInteractionEnabled =YES;
-    _img3.contentMode = UIViewContentModeScaleAspectFit;
-    [_img3 addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tagclick:)]];
-    
-    [_img4 JLloadimgwith:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=474291294,2674600622&fm=202&mola=new&crop=v1" andplaceholderImage:@"12345"];
-    //[_img4 sd_setImageWithURL:[NSURL URLWithString:@"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=474291294,2674600622&fm=202&mola=new&crop=v1"]];
-    _img4.userInteractionEnabled =YES;
-    _img4.contentMode = UIViewContentModeScaleAspectFit;
-    [_img4 addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tagclick:)]];
-    
-    UIPanGestureRecognizer *panges = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(JLPhotoPress:)];
-    panges.delegate = self;
-    [self.view addGestureRecognizer: panges ];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 -(void)tagclick:(UITapGestureRecognizer *)tag{
-//    testtableViewController *vc =[[testtableViewController alloc]init];
-//    [self presentViewController:vc animated:YES completion:^{
-//
-//    }];
-    
-   BOOL flag = [[JLImgCache sharedImageCache] clearMemory];
-    if (flag) {
-        NSLog(@"yes");
-    }else{
-        NSLog(@"no");
-    }
-//    UIView *view =tag.view;
-//    JLPhotoBrowser *view1 =[[JLPhotoBrowser alloc] initWithFrame:self.view.window.frame];
-//    view1.fristView =view;
-//    view1.placeholderImage =@"placeholder";
-//    view1.ImageUrlArr =@[@"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=3545401083,2251952841&fm=77&w_h=121_75&cs=2008984878,1171842780",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=3618554304,2887917621&fm=77&w_h=121_75&cs=2820658166,1330608299",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=1618097094,4154452434&fm=77&w_h=121_75&cs=423647557,799948659"];
-//    view1.Current =view.tag-100;
-//    [view1 show:^(BOOL issure, NSInteger currentpage) {
-//        //是否返回，从第几页返回
-//    }];
-//
-//    [self.view.window addSubview:view1];
 
-    
-    
-    
-//    JLPhotoBrowser *view =[[JLPhotoBrowser alloc] initWithFrame:self.view.window.frame];
-//    view.fristView =tag.view;
-//    view.placeholderImage =@"placeholder";
-//    view.ImageUrlArr =@[@"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1972873509,2904368741&fm=27&gp=0.jpg",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=3545401083,2251952841&fm=77&w_h=121_75&cs=2008984878,1171842780",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=3618554304,2887917621&fm=77&w_h=121_75&cs=2820658166,1330608299",@"https://ss0.bdstatic.com/6ONWsjip0QIZ8tyhnq/it/u=1618097094,4154452434&fm=77&w_h=121_75&cs=423647557,799948659"];
-//    view.Current =tag.view.tag-100;
-//    [view show:^(BOOL issure, NSInteger currentpage) {
-//        //是否返回，从第几页返回
-//    }];
-//    [self.view.window addSubview:view];
 }
 
 -(void)JLPhotoPress:(UIGestureRecognizer *)tag{
